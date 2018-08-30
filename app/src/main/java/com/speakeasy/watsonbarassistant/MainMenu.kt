1a186c31e8e33d27ea10220668b2ce38e09727ad
Yuxiang Lin <lin.2453@osu.edu>
2018-08-30 16:31:25 -0400

    Initial Setup

---
+++ app/src/main/java/com/speakeasy/watsonbarassistant/MainMenu.kt
@@ -0,0 +1,12 @@
+package com.speakeasy.watsonbarassistant
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainMenu : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_menu)
+    }
+}
