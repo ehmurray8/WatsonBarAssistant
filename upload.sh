6df7a31be9d655c57fc20a651479fef0fb8f3bf1
Emmet Murray <ehmurray8@gmail.com>
2018-09-12 19:30:00 -0400

    Added upload shell.

---
+++ upload.sh
@@ -0,0 +1,2 @@
+curl -F "status=2" -F "notify=1" -F "notes=Some new features and fixed bugs." -F "notes_type=0" \
+    -F "ipa=@app/build/outputs/apk/app-debug.apk" -H "X-HockeyAppToken: $HOCKEYAPP_TOKEN" https://rink.hockeyapp.net/api/2/apps/upload
