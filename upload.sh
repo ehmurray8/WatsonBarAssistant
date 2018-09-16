5a94613d60a05311c0ad08b055767189ce800bd3
Emmet Murray <ehmurray8@gmail.com>
2018-09-16 13:10:13 -0400

    Fixed Hockey apps notes generation.

--- upload.sh
+++ upload.sh
@@ -1,4 +1,4 @@
 curl -F "status=2" -F "notify=1" \
--F "notes=Branch: $TRAVIS_BRANCH\nLast Commit: $TRAVIS_COMMIT $TRAVIS_COMMIT_MESSAGE\nPull Request: $TRAVIS_PULL_REQUEST_BRANCH - Origin($TRAVIS_PULL_REQUEST_BRANCH)\nTravis Build Number: $TRAVIS_BUILD_NUMBER" \
+-F "notes=Branch: $TRAVIS_BRANCH | Pull Request: $TRAVIS_PULL_REQUEST_BRANCH | Last Commit: $TRAVIS_COMMIT $TRAVIS_COMMIT_MESSAGE | Travis Build Number: $TRAVIS_BUILD_NUMBER" \
 -F "notes_type=0" -F "ipa=@app/build/outputs/apk/debug/app-debug.apk" \
 -H "X-HockeyAppToken: $HOCKEYAPP_TOKEN" "https://rink.hockeyapp.net/api/2/apps/upload"
