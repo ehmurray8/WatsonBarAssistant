8683561861f46270d25431a5aa44cbaf7926a0c4
Emmet Murray <ehmurray8@gmail.com>
2018-09-16 12:46:45 -0400

    Added notes to the hockey app release.

--- upload.sh
+++ upload.sh
@@ -1,1 +1,4 @@
-curl -F "status=2" -F "notify=1" -F "ipa=@app/build/outputs/apk/debug/app-debug.apk" -H "X-HockeyAppToken: $HOCKEYAPP_TOKEN" "https://rink.hockeyapp.net/api/2/apps/upload"
+curl -F "status=2" -F "notify=1" \
+-F "notes=Branch: $TRAVIS_BRANCH\nLast Commit: $TRAVIS_COMMIT $TRAVIS_COMMIT_MESSAGE\nPull Request: $TRAVIS_PULL_REQUEST_BRANCH - Origin($TRAVIS_PULL_REQUEST_BRANCH)\nTravis Build Number: $TRAVIS_BUILD_NUMBER" \
+-F "notes_type=0" -F "ipa=@app/build/outputs/apk/debug/app-debug.apk" \
+-H "X-HockeyAppToken: $HOCKEYAPP_TOKEN" "https://rink.hockeyapp.net/api/2/apps/upload"
