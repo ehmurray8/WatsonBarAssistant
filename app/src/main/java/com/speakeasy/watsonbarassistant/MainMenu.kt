3117ab97438802b38d5b9df4992ea1be4c707727
Emmet Murray <ehmurray8@gmail.com>
2018-10-04 22:44:55 -0400

    Fixed recipe caching. (#17)

--- app/src/main/java/com/speakeasy/watsonbarassistant/MainMenu.kt
+++ app/src/main/java/com/speakeasy/watsonbarassistant/MainMenu.kt
@@ -82,8 +82,8 @@ class MainMenu : AppCompatActivity() {
         return true
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
+    override fun onPause() {
+        super.onPause()
         val preferences = getSharedPreferences(SHARED_PREFERENCES_SETTINGS, Context.MODE_PRIVATE)
         val editor = preferences.edit()
         val gson = Gson()
@@ -106,9 +106,9 @@ class MainMenu : AppCompatActivity() {
         refreshDiscovery()
         val oldIngredients = ingredients.toTypedArray()
         if(uid != null) {
-            ingredients.clear()
             fireStore.collection("app").document(uid)
                     .collection("ingredients").get().addOnCompleteListener {
+                ingredients.clear()
                 if (it.isSuccessful) {
                     it.result.forEach { snapshot ->
                         parseSnapshot(snapshot)
