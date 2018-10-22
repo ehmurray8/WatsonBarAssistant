f85d9d06fa1130ac9f2201414a9f1a48ca5cf17b
LaneLucas <lucas.612@buckeyemail.osu.edu>
2018-10-21 20:27:55 -0400

    Commit to test

--- app/src/test/java/com/speakeasy/watsonbarassistant/Discovery/SearchDiscoveryTest.kt
+++ app/src/test/java/com/speakeasy/watsonbarassistant/Discovery/SearchDiscoveryTest.kt
@@ -1,18 +1,33 @@
 package com.speakeasy.watsonbarassistant.Discovery
 
+import com.ibm.watson.developer_cloud.discovery.v1.model.QueryResponse
 import com.speakeasy.watsonbarassistant.Ingredient
 import com.speakeasy.watsonbarassistant.MainMenu
 import junit.framework.TestCase
 import org.junit.Assert
+import org.junit.Before
 import org.junit.Test
+import org.mockito.Mock
 import org.mockito.Mockito.mock
+import org.mockito.MockitoAnnotations
 
 class SearchDiscoveryTest: TestCase() {
 
+    lateinit var searchDiscovery: SearchDiscovery
+    @Mock
+    lateinit var handleDiscovery: HandleDiscovery
+    @Mock
+    lateinit var queryResponse: QueryResponse
+
+    @Before
+    override fun setUp(){
+        MockitoAnnotations.initMocks(this)
+
+        searchDiscovery = SearchDiscovery(handleDiscovery)
+    }
+
     @Test
-    fun testcreateAndSetsBasicTest() {
-        val mainMock = mock(MainMenu().javaClass)
-        val searchDiscovery = SearchDiscovery(HandleDiscovery(mainMock))
+    fun testCreateAndSetsBasic() {
         var ingredients = arrayOf<Ingredient>()
         ingredients[0] = Ingredient("a")
 
